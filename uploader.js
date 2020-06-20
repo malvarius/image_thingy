0049da6fa926d925fc27ca964ce9c8a4e15ba46b
malvarius <e.luismalvar@gmail.com>
2020-06-20 13:15:41 -0700

    adding base64 converter

--- uploader.js
+++ uploader.js
@@ -1,20 +1,32 @@
-let count = 0;
+//The image hide is simply to keep the blank image from showing until it is populated in the even listener function
 if($('img').attr("src")==="#"){
     $(this).hide()
 }
+//Function on file load to take the blank image tag and set src to the bubble URl from file upload so you can see the image you are converting
 window.addEventListener('load', function() {
     $('input[type=file]').on('change',function(){
         if (this.files && this.files[0]) {
+            const url = URL.createObjectURL(this.files[0])
             $(".showMe").show()
-            $(".showMe").append(`<br><div class="showMe" style="justify-content: center;"><img id="myImg${count+1}" src=""  class="img-fluid d-block mx-auto"></div><br>`)
-            $(`#myImg${count}`).attr("src", URL.createObjectURL(this.files[0]))
-            console.log(URL.createObjectURL(this.files[0]))
-            count++
+            $(`#myImg`).attr("src", url)
+            convertToBase64(this.files[0])
         }
     });
   });
-  
-  function imageIsLoaded() { 
-    alert(this.src);  // blob url
-    // update width and height ...
-  }
+  //function that takes in the file (labeled as info) and reads the bubble URL to convert to base64
+  function convertToBase64(info) {
+    var readMe = new FileReader();
+    readMe.readAsDataURL(info);
+    readMe.onload = function () {
+      $("#base").val(readMe.result)
+    };
+    readMe.onerror = function (error) {
+      return "There was an error with your file"
+    };
+ }
+//Function to allow easier copying by simply clicking the copy button at top of text area
+ $("#copyButton").on('click', function(){
+   const value = $("#base").val();
+   navigator.clipboard.writeText(value)
+
+ })
